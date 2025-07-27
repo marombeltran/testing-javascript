dedbe5b230814ce1b53414643961827f3bb666c0
Marom Beltran <mrmbeltran@proton.me>
2025-07-27 14:13:40 -0600

    🔥 Cleanup: remove ignored files from git tracking

---
+++ lessons/assertion-library.js
@@ -0,0 +1,28 @@
+const {sum, subtract} = require('./math');
+
+let result, expected
+
+result = sum(3, 7)
+expected = 10
+expect(result).toBe(expected)
+// if (result !== expected) {
+	// throw new Error(`${result} is not equal to ${expected}`)
+// }
+
+result = subtract(7, 3)
+expected = 4
+expect(result).toBe(expected)
+
+// if (result !== expected) {
+	// throw new Error(`${result} is not equal to ${expected}`)
+// }
+
+function expect (actual) {
+	return  {
+		toBe(expected) {
+			if (result !== expected) {
+				throw new Error(`${result} is not equal to ${expected}`)
+			}
+		}
+	}
+}
